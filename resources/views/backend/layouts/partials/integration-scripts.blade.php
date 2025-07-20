aca404ed1610fb494060d9c64543042978a67974
Hasan-Raza-InterSys <hasanintersysltd@gmail.com>
2025-07-20 06:04:22 +0500

    remove bug

--- resources/views/backend/layouts/partials/integration-scripts.blade.php
+++ resources/views/backend/layouts/partials/integration-scripts.blade.php
@@ -15,7 +15,6 @@
         gtag("js", new Date());
         gtag("config", "G-WWCRYQMHZ7");
     </script>
-<!-- @else if (!empty(config('settings.google_analytics_script'))) -->
     {!! config('settings.google_analytics_script') !!}
 @endif
 
